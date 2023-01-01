f2fbc27811fac8a74b269e48fecdbe8ee5b8cb33
MOHD AMIR AHMAD <amir.engineer786@gmail.com>
2023-01-01 23:17:11 +0530

    commit

--- SpringDataRestExample/src/main/java/com/example/demo/SpringDataRestExampleApplication.java
+++ SpringDataRestExample/src/main/java/com/example/demo/SpringDataRestExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringDataRestExampleApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringDataRestExampleApplication.class, args);
 	}
 
